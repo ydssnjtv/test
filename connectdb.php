7c7d5787ccc7522b8db04320df01b1fe2c3c55a2
ydss <ydss@163.com>
2016-03-10 17:05:24 +0800

    Signed-off-by: ydss <ydss@163.com>

--- connectdb.php
+++ connectdb.php
@@ -1,12 +1,13 @@
 ﻿<?php
 function linkdb(){
 //连接新浪云主站及数据库
-$hostname = '192.168.1.11'.':'.'3306';
+$hostname = '192.168.1.11';
+$dbport = '3306'
 $dbuser = "7f1a7a89-fe9b";
 $dbpass = "368a98a2-2d68";
 $dbname = "dfd61754ba8e04374a91b8bdf5344e36a";
 //$link = mysql_connect($hostname, $dbuser, $dbpass);   ~~~mysql_connect过时了，被下面的mysqli_connect取代，但是后者的参数定义不一样的，端口放在最后！！
-$link = mysqli_connect(SAE_MYSQL_HOST_M, SAE_MYSQL_USER, SAE_MYSQL_PASS, SAE_MYSQL_DB, SAE_MYSQL_PORT);
+$link = mysqli_connect($hostname, $dbuser, $dbpass, $dbname, $dbport);
 //mysql_query("SET NAMES UTF8");  据说是解决乱码的
 if (!$link) {die('Could not connect: '.mysql_error());}
 //echo "成功连接新浪云主机！<br/>";
