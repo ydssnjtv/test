4ad55df00450daffd1f2ec6b480395f73e3f25fe
ydss <ydss@163.com>
2016-03-11 16:23:47 +0800

    Signed-off-by: ydss <ydss@163.com>

--- nfs.php
+++ nfs.php
@@ -1,5 +1,6 @@
 <?php
-$path = getenv('PATH');
+//$path = getenv('PATH');
+$path = "/home/vcap/file/e195c08b-c44c-48c4-ab33-cf6fd62a7e9d";
 if(file_exists($path)){
     echo 'FileSystem is effective.';
 }
