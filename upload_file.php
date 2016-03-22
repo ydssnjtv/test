bdf32f2c988eb031912cc3a748ef8349ea8fb677
ydss <ydss@163.com>
2016-03-22 17:18:13 +0800

    Signed-off-by: ydss <ydss@163.com>

--- upload_file.php
+++ upload_file.php
@@ -1,4 +1,6 @@
 <?PHP
+
+$dir = "/home/vcap/file/e195c08b-c44c-48c4-ab33-cf6fd62a7e9d/";
 if ($_FILES["file"]["error"] > 0)
   {
   echo "Error: " . $_FILES["file"]["error"] . "<br />";
@@ -8,6 +10,17 @@ else
   echo "Upload: " . $_FILES["file"]["name"] . "<br />";
   echo "Type: " . $_FILES["file"]["type"] . "<br />";
   echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+  echo "Stored in: " . $_FILES["file"]["tmp_name"].'<br/>';
+  
+  if (file_exists($dir . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      $dir . $_FILES["file"]["name"]);
+      echo "Stored as: " . $_FILES["file"]["name"];
+      }
   }
 ?>
