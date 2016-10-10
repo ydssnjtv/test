5eb5a01efc5929ac1983c9f2bd0a2f305f6ad0ca
ydss <ydss@163.com>
2016-10-10 16:16:54 +0800

    Signed-off-by: ydss <ydss@163.com>

--- weixin.php
+++ weixin.php
@@ -43,14 +43,40 @@ class wechatCallbackapiTest
     public function responseMsg()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
+        //接受用户端发送过来的xml数据
         if (!empty($postStr)){
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
             $time = time();
-
+            //通过simplexml进行xml解析
+	        
+			$receive_msgtype = $postObj->MsgType;
+			if ($receive_msgtype == "event")
+			{
+				$receive_event = $postobj->event;
+				if ($receive_event == 'Subcribe')
+				{
+					$textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+				$msgType = "text";
+                $contentStr = "欢迎订阅Ydss的公众号，由于微信取消了自定义菜单，请输入任意字符以获取文本操作菜单";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+				exit;
+				}
+			exit;
+			}
+		    //如果是新用户订阅消息，则返回欢迎和菜单消息
+			
+		
             if($keyword == "1")
 			{
 				$textTpl = "<xml>
@@ -74,6 +100,8 @@ class wechatCallbackapiTest
                 echo $resultStr;
 				exit;
 			}
+			//如果对方输入“1”则返回班表页面；
+			
 			
 			if($keyword == "2")
 			{
@@ -98,6 +126,8 @@ class wechatCallbackapiTest
                 echo $resultStr;
 				exit;
 			}
+			//如果对方输入“2”则返回日历页面；
+			
 			
 			if($keyword <> null)
             {
@@ -115,7 +145,7 @@ class wechatCallbackapiTest
                 echo $resultStr;
 				exit;
             }
-			
+			//如果对方输入其他文本则返回日历页面；
         }
     }
 }
