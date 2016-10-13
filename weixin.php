9d07279db8df603f2028f732596b208020621a75
ydss <ydss@163.com>
2016-10-13 17:10:31 +0800

    Signed-off-by: ydss <ydss@163.com>

--- weixin.php
+++ weixin.php
@@ -133,7 +133,7 @@ class wechatCallbackapiTest
             case "subscribe": $contentStr = "感谢您关注Ydss的微信号"."\n"."目前平台功能如下："."\n"."【1】 查班表和值班量"."\n"."【2】 日历"."\n"."【3】 更多内容，敬请期待...";
                 break;
 			case "LOCATION": $contentStr = "纬度 ".$object->Latitude." 经度".$object->Longitude;
-			    break;
+			    break;//这里想输出对方的地理位置,不成功;
 			default : $contentStr = "Unknow Event: ".$postobj->Event;
                 break;
         }
