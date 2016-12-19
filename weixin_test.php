9c1fc10a6709000987ad3b25ab7c1851b35d20da
ydss <ydss@163.com>
2016-12-19 17:11:00 +0800

    Signed-off-by: ydss <ydss@163.com>

--- weixin_test.php
+++ weixin_test.php
@@ -2,155 +2,20 @@
 
 define("TOKEN", "ydssnjtv");
 
+$signature = $_GET["signature"];
+$echostr = $_GET["echostr"];
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];
 
+$tmpArr = array($nonce, $timestamp, TOKEN);
+sort($tmpArr);
 
-
-
-
-//$wechatObj = new wechatCallbackapiTest();
-//$wechatObj->valid();
-$wechatObj->responseMsg();
-$wechatObj->valid(); 
-
-class wechatCallbackapiTest
-{
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
-
-        //valid signature , option
-        if($this->checkSignature()){
-            ob_clean();
-			echo $echoStr;
-            exit;
-        }
-    }
-	
-	public function responseMsg()
-    {
-        //get post data, May be due to the different environments 
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        //extract post data 
-		if (!empty($postStr)){
-                
-                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $RX_TYPE = trim($postObj->MsgType);
-
-                switch($RX_TYPE)
-                {
-                    case "text": $resultStr = $this->handleText($postObj);
-                        break;
-                    case "event": $resultStr = $this->handleEvent($postObj);
-                        break;
-                    default: $resultStr = "Unknow msg type: ".$RX_TYPE;
-                        break;
-                }
-                echo $resultStr;
-        }else {
-            echo "";
-            exit;
-        }
-    }
-
-    public function handleText($postObj)
-    {
-        $fromUsername = $postObj->FromUserName;
-        $toUsername = $postObj->ToUserName;
-        $keyword = trim($postObj->Content);
-        $time = time();
-        $textTpl = "<xml>
-                    <ToUserName><![CDATA[%s]]></ToUserName>
-                    <FromUserName><![CDATA[%s]]></FromUserName>
-                    <CreateTime>%s</CreateTime>
-                    <MsgType><![CDATA[%s]]></MsgType>
-                    <Content><![CDATA[%s]]></Content>
-                    <FuncFlag>0</FuncFlag>
-                    </xml>";             
-        if(!empty( $keyword ))
-        {
-            $msgType = "text";
-            $contentStr = "Welcome to wechat world!";
-            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-            echo $resultStr;
-        }else{
-            echo "Input something...";
-        }
-    }
-
-    public function handleEvent($object)
-    {
-        $contentStr = "";
-        switch ($object->Event)
-        {
-            case "subscribe": $contentStr = "感谢您关注Ydss的微信号"."\n"."目前平台功能如下："."\n"."【1】 查班表和值班量"."\n"."【2】 日历"."\n"."【3】 更多内容，敬请期待...";
-                break;
-            default : $contentStr = "Unknow Event: ".$object->Event;
-                break;
-        }
-        $resultStr = $this->responseText($object, $contentStr);
-        return $resultStr;
-    }
-    
-    public function responseText($object, $content, $flag=0)
-    {
-        $textTpl = "<xml>
-                    <ToUserName><![CDATA[%s]]></ToUserName>
-                    <FromUserName><![CDATA[%s]]></FromUserName>
-                    <CreateTime>%s</CreateTime>
-                    <MsgType><![CDATA[text]]></MsgType>
-                    <Content><![CDATA[%s]]></Content>
-                    <FuncFlag>%d</FuncFlag>
-                    </xml>";
-        $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content, $flag);
-        return $resultStr;
-    }
-
-//    private function checkSignature()
-//    {
-//        $signature = $_GET["signature"];
-//        $timestamp = $_GET["timestamp"];
-//        $nonce = $_GET["nonce"];    
-//                
-//        $token = TOKEN;
-//        $tmpArr = array($token, $timestamp, $nonce);
-//        sort($tmpArr);
-//        $tmpStr = implode( $tmpArr );
-//        $tmpStr = sha1( $tmpStr );
-//        
-//        if( $tmpStr == $signature ){
-//            return true;
-//        }else{
-//            return false;
-//        }
-//    }
-
-private function checkSignature()
-
-{
-
-        $signature = $_GET["signature"];
-
-        $timestamp = $_GET["timestamp"];
-
-        $nonce = $_GET["nonce"];
-
-        
-
-$token = "ydssnjtv";
-
-$tmpArr = array($token, $timestamp, $nonce);
-
-sort($tmpArr, SORT_STRING);
-
-$tmpStr = implode( $tmpArr );
-
-$tmpStr = sha1( $tmpStr );
-
+$tmpStr = implode($tmpArr);
+$tmpStr = sha1($tmpStr);
 
 if( $tmpStr == $signature ){
 
-return true;
+echo $echostr;
 
 }else{
 
@@ -158,9 +23,9 @@ return false;
 
 }
 
-}
 
 
-}
+
+
 
 ?>
