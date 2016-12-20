e175922297a61c5444fee67f33d10c3c3a2915fe
ydss <ydss@163.com>
2016-12-20 17:42:43 +0800

    Signed-off-by: ydss <ydss@163.com>

--- readme.txt
+++ readme.txt
@@ -3,3 +3,7 @@ result.php--排班表
 zhibanliang.php--值班量
 panduan--判断输入日期是否有效
 connectdb.php--连接数据库
+
+weixin.php--mopaas v2可以，v3就token验证不过
+weixin_test.php--mopaas v3可以用
+weixin_test_backup.php mopaas v3验证token的最简代码
--- zhibanliang_year.php
+++ zhibanliang_year.php
@@ -26,7 +26,7 @@ $year=$_POST['year'];
 //echo $year.'年'.$month.'月<br/><br/>';
 $time=date("Y-m-d",mktime(0,0,0,1,1,$year));
 $timeend1=date("Y-m-d",mktime(0,0,0,12,31,$year));
-echo "<p style='color:red;font-size:32px;text-align:center;'>你选择的时间是：".$year.'年'.$month.'月<br/></p>';
+echo "<p style='color:red;font-size:32px;text-align:center;'>你选择的时间是：".$year.'年<br/></p>';
 panduan($time);
 //具体查询和结果显示
 //$sql_zhongban= "SELECT `中班`,count(*) from `banbiao` WHERE `日期` BETWEEN '$time' AND '$timeend1' group by `中班`";//sql语句里的表和字段要用``括起来，字符串和变量要用单引号括起来；
