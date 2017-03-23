6accc0e566c4e9c1a081abd299371cc3f8179024
ydss <ydss@163.com>
2017-03-23 10:13:13 +0800

    Signed-off-by: ydss <ydss@163.com>

--- zhibanliang_qianjing.php
+++ zhibanliang_qianjing.php
@@ -1,15 +1,15 @@
 ﻿<table border="1" align="center">
  <tr>
-  <th><span style='color:black;font-size:35px'>姓名</span></th>
-  <th><span style='color:black;font-size:35px'>吴锦夏</span></th>
-  <th><span style='color:black;font-size:35px'>胡守霞</span></th>
-  <th><span style='color:black;font-size:35px'>徐艳</span></th>
-  <th><span style='color:black;font-size:35px'>孙海燕</span></th>
-  <th><span style='color:black;font-size:35px'>郑雯</span></th>
-  <th><span style='color:black;font-size:35px'>杨芮</span></th>
-  <th><span style='color:black;font-size:35px'>钱静</span></th>
-  <th><span style='color:black;font-size:35px'>杨旸</span></th>
-  <th><span style='color:black;font-size:35px'>李笑啡</span></th>
+  <th><span style='color:black;font-size:32px'>姓名</span></th>
+  <th><span style='color:black;font-size:32px'>吴锦夏</span></th>
+  <th><span style='color:black;font-size:32px'>胡守霞</span></th>
+  <th><span style='color:black;font-size:32px'>徐艳</span></th>
+  <th><span style='color:black;font-size:32px'>孙海燕</span></th>
+  <th><span style='color:black;font-size:32px'>郑雯</span></th>
+  <th><span style='color:black;font-size:32px'>杨芮</span></th>
+  <th><span style='color:black;font-size:32px'>钱静</span></th>
+  <th><span style='color:black;font-size:32px'>杨旸</span></th>
+  <th><span style='color:black;font-size:32px'>李笑啡</span></th>
  </tr>
 <?php
 require_once('panduan.php');
@@ -36,91 +36,91 @@ panduan($time);
 $renyuan=array('吴锦夏','胡守霞','徐艳','孙海燕','郑雯','杨芮','钱静','杨旸','李笑啡');
 $summary=array('吴锦夏'=>0,'胡守霞'=>0,'徐艳'=>0,'孙海燕'=>0,'郑雯'=>0,'杨芮'=>0,'钱静'=>0,'杨旸'=>0,'李笑啡'=>0);
 //下面通过5次查询数据库，得出5种班次的各人统计；
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center'>早班</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center'>早班</p></td>";
 $i=0;
 while($i<=8){
   $ry=$renyuan[$i];
   $sql_ss = "select count(*) from `banbiaoqianjing` where `日期` BETWEEN '$time' AND '$timeend1' and `早班` like '%$ry%'";//select语句里变量要用引号括起来！！！！这句执行的结果是某人本月所有早班的列表；
   $aa=mysql_query($sql_ss)or die("对不起，读入数据时出错了！". mysql_error());
   $count=mysql_fetch_row($aa);
-   echo "<td><p style='color:black;font-size:35px;text-align:center'>".$count[0]."</p></td>";
+   echo "<td><p style='color:black;font-size:32px;text-align:center'>".$count[0]."</p></td>";
    $summary[$ry]=$summary[$ry]+$count[0];//这是计算最后每人合计总班次的；
    if($i==7){echo "</tr>";}//再次提醒！！！如果写$i=7就不对，比较关系中只有==，不能=；
    $i++;
 }
 
 
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center'>中班</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center'>中班</p></td>";
 $i=0;
 while($i<=8){
   $ry=$renyuan[$i];
   $sql_ss = "select count(*) from `banbiaoqianjing` where `日期` BETWEEN '$time' AND '$timeend1' and `中班` like '%$ry%'";//select语句里变量要用引号括起来！！！！这句执行的结果是某人本月所有早班的列表；
   $aa=mysql_query($sql_ss)or die("对不起，读入数据时出错了！". mysql_error());
   $count=mysql_fetch_row($aa);
-   echo "<td><p style='color:black;font-size:35px;text-align:center'>".$count[0]."</p></td>";
+   echo "<td><p style='color:black;font-size:32px;text-align:center'>".$count[0]."</p></td>";
    $summary[$ry]=$summary[$ry]+$count[0];
    if($i==7){echo "</tr>";}
    $i++;
 }
 
 
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center'>中二</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center'>中二</p></td>";
 $i=0;
 while($i<=8){
   $ry=$renyuan[$i];
   $sql_ss = "select count(*) from `banbiaoqianjing` where `日期` BETWEEN '$time' AND '$timeend1' and `中二` like '%$ry%'";//select语句里变量要用引号括起来！！！！这句执行的结果是某人本月所有早班的列表；
   $aa=mysql_query($sql_ss)or die("对不起，读入数据时出错了！". mysql_error());
   $count=mysql_fetch_row($aa);
-   echo "<td><p style='color:black;font-size:35px;text-align:center'>".$count[0]."</p></td>";
+   echo "<td><p style='color:black;font-size:32px;text-align:center'>".$count[0]."</p></td>";
    $summary[$ry]=$summary[$ry]+$count[0];
    if($i==7){echo "</tr>";}
    $i++;
 }
 
 
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center'>小晚</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center'>小晚</p></td>";
 $i=0;
 while($i<=8){
   $ry=$renyuan[$i];
   $sql_ss = "select count(*) from `banbiaoqianjing` where `日期` BETWEEN '$time' AND '$timeend1' and `小晚` like '%$ry%'";//select语句里变量要用引号括起来！！！！这句执行的结果是某人本月所有早班的列表；
   $aa=mysql_query($sql_ss)or die("对不起，读入数据时出错了！". mysql_error());
   $count=mysql_fetch_row($aa);
-   echo "<td><p style='color:black;font-size:35px;text-align:center'>".$count[0]."</p></td>";
+   echo "<td><p style='color:black;font-size:32px;text-align:center'>".$count[0]."</p></td>";
    $summary[$ry]=$summary[$ry]+$count[0];
    if($i==7){echo "</tr>";}
    $i++;
 }	   
 
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center'>审晚</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center'>审晚</p></td>";
 $i=0;
 while($i<=8){
   $ry=$renyuan[$i];
   $sql_ss = "select count(*) from `banbiaoqianjing` where `日期` BETWEEN '$time' AND '$timeend1' and `审晚` like '%$ry%'";
   $aa=mysql_query($sql_ss)or die("对不起，读入数据时出错了！". mysql_error());
   $count=mysql_fetch_row($aa);
-   echo "<td><p style='color:black;font-size:35px;text-align:center'>".$count[0]."</p></td>";
+   echo "<td><p style='color:black;font-size:32px;text-align:center'>".$count[0]."</p></td>";
    $summary[$ry]=$summary[$ry]+$count[0];
    if($i==7){echo "</tr>";}
    $i++;
 }
 
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center'>收带</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center'>收带</p></td>";
 $i=0;
 while($i<=8){
   $ry=$renyuan[$i];
   $sql_ss = "select count(*) from `banbiaoqianjing` where `日期` BETWEEN '$time' AND '$timeend1' and `收带` like '%$ry%'";
   $aa=mysql_query($sql_ss)or die("对不起，读入数据时出错了！". mysql_error());
   $count=mysql_fetch_row($aa);
-   echo "<td><p style='color:black;font-size:35px;text-align:center'>".$count[0]."</p></td>";
+   echo "<td><p style='color:black;font-size:32px;text-align:center'>".$count[0]."</p></td>";
    $summary[$ry]=$summary[$ry]+$count[0];
    if($i==7){echo "</tr>";}
    $i++;
 }
 
-echo "<tr><td><p style='color:black;font-size:35px;text-align:center;font-weight:bold'>合计</p></td>";
+echo "<tr><td><p style='color:black;font-size:32px;text-align:center;font-weight:bold'>合计</p></td>";
 $i=0;
 foreach($summary as $cc=>$vv){
-    echo "<td><p style='color:black;font-size:35px;text-align:center;font-weight:bold'>".$vv."</p></td>";
+    echo "<td><p style='color:black;font-size:32px;text-align:center;font-weight:bold'>".$vv."</p></td>";
 	if($i==8){echo "</tr>";}
    $i++;
 }
